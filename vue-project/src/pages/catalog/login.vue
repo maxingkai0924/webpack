7de8bbfedbd8742055cf4f4ca9bab62a970b7662
maxingkai <939898730@qq.com>
2020-04-24 14:54:54 +0800

    玩

--- vue-project/src/pages/catalog/login.vue
+++ vue-project/src/pages/catalog/login.vue
@@ -10,13 +10,15 @@ export default {
     }
   },
   mounted () {
+    console.log(22)
     this.$axios({
       method: 'get',
       url: '/sdm/back/index/getMessage',
       data: {},
       maxRedirects: 10
     }).then((res) => {
-      // console.log(res)
+     
+      console.log(res)
     })
   }
 }
